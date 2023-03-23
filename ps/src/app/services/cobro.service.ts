a3a18f740cd18464422784db3a81069558a248b5
2w1-113491-Monticoli Pablo <113491@tecnicatura.frc.utn.edu.ar>
2023-03-23 12:28:33 -0300

    creo e implemento servicio cobro

---
+++ ps/src/app/services/cobro.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResultadoGenerico } from '../models/resultado-generico';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CobroService {
+  private API_URL: string = 'http://localhost:3000/cobros/';
+  constructor(private http : HttpClient) { }
+
+  agregar (body : any) : Observable<ResultadoGenerico>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({     
+      'Content-Type': 'application/json',
+
+      'Authorization': `Bearer ${auth_token}`
+    })
+    const requestOptions= {headers : headers};
+    return this.http.post<ResultadoGenerico>(this.API_URL,requestOptions);
+  }
+}
