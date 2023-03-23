bb8a3cb6d2dcca7334dd1f7202444ea1e8491e27
2w1-113491-Monticoli Pablo <113491@tecnicatura.frc.utn.edu.ar>
2023-03-23 12:53:55 -0300

    creo e implemento servicio tipo de pago

---
+++ ps/src/app/services/tipo-pago.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResultadoGenerico } from '../models/resultado-generico';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TipoPagoService {
+  private API_URL : string = 'http://localhost/3000/tiposPago/';
+  constructor(private http : HttpClient) { }
+
+  obtenerTodos() : Observable<ResultadoGenerico>{
+    return this.http.get<ResultadoGenerico>(this.API_URL);
+  }
+}
