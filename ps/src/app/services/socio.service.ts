6999d88cad6bccd53f9ea0fdb85bfc88f8e79fe9
2w1-113491-Monticoli Pablo <113491@tecnicatura.frc.utn.edu.ar>
2023-03-24 11:10:34 -0300

    creo e implemento servicio socio

---
+++ ps/src/app/services/socio.service.ts
@@ -0,0 +1,83 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResultadoGenerico } from '../models/resultado-generico';
+import { Socio } from '../models/socio';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocioService {
+  private API_URL : string = 'http://localhost:3000/socios/';
+  constructor(private http : HttpClient) { }
+
+  agregar (socio : Socio) : Observable<ResultadoGenerico>{
+    return this.http.post<ResultadoGenerico>(this.API_URL,socio);
+  }
+
+  obtenerTodos(): Observable<ResultadoGenerico>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      });
+    const requestOptions = { headers: headers };
+    return this.http.get<ResultadoGenerico>(this.API_URL,requestOptions);
+  }
+
+  eliminar(socio : Socio) : Observable<any>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      });
+    const requestOptions = { headers: headers }
+    return this.http.delete(this.API_URL+socio.id,requestOptions);
+  }
+
+  obtenerDetallesSocio(idSocio: number): Observable<ResultadoGenerico> {
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`
+    });
+    const requestOptions = { headers: headers };
+    return this.http.get<ResultadoGenerico>(this.API_URL + idSocio, requestOptions);
+  }
+
+  modificar(body : Socio) : Observable<ResultadoGenerico>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      });
+    const requestOptions = { headers: headers };
+    return this.http.put<ResultadoGenerico>(this.API_URL,body,requestOptions);
+   }
+
+   getSocioById(idSocio : number) : Observable<ResultadoGenerico> {
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      });
+    const requestOptions = { headers: headers };
+      return this.http.get<ResultadoGenerico>(this.API_URL +idSocio,requestOptions);
+   }
+
+   
+   obtenerPuntosDeSocio() : Observable<ResultadoGenerico> {
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      });
+    const requestOptions = { headers: headers };
+      return this.http.get<ResultadoGenerico>(this.API_URL+'puntos',requestOptions);
+   }
+
+
+   existeSocioConDNI(dni:number): Observable<boolean> {
+    return this.http.get<boolean>(this.API_URL+'existe?dni='+dni);
+   }
+}
