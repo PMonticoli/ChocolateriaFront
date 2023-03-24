c435e7cd4f55b0ce9f895c60ec5783c2b59a4244
2w1-113491-Monticoli Pablo <113491@tecnicatura.frc.utn.edu.ar>
2023-03-24 11:09:59 -0300

    agrego imports del componente socio y  socio al navbar

--- ps/src/app/app-routing.module.ts
+++ ps/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AltaPedidoComponent } from './components/pedidos/alta-pedido/alta-pedid
 import { ListadoPedidosPendientesComponent } from './components/pedidos/listados/listado-pedidos-pendientes/listado-pedidos-pendientes.component';
 import { ListadoPedidosPropiosComponent } from './components/pedidos/listados/listado-pedidos-propios/listado-pedidos-propios.component';
 import { ListadoPedidosComponent } from './components/pedidos/listados/listado-pedidos/listado-pedidos.component';
+import { AltaSocioComponent } from './components/socios/alta-socio/alta-socio.component';
 
 const routes: Routes = [
   {path : 'home', component : HomeComponent},
@@ -14,7 +15,8 @@ const routes: Routes = [
   {path : 'pedido/nuevo', component: AltaPedidoComponent},
   {path: 'pedido/listado', component: ListadoPedidosComponent},
   {path: 'pedido/pendiente', component: ListadoPedidosPendientesComponent},
-  {path: 'pedido/propios', component: ListadoPedidosPropiosComponent}
+  {path: 'pedido/propios', component: ListadoPedidosPropiosComponent},
+  {path: 'socio/nuevo', component: AltaSocioComponent}
 ];
 
 @NgModule({
