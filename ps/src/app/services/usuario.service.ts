8e7efce605cb0f4b9abaf39d13ac9023168f9d28
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-03-29 11:53:07 -0300

    agrego metodo obtenerRol

--- ps/src/app/services/usuario.service.ts
+++ ps/src/app/services/usuario.service.ts
@@ -2,15 +2,24 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ResultadoGenerico } from '../models/resultado-generico';
 import { UsuarioLogin } from '../models/usuario-login';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class UsuarioService {
 
-  private recurso : string = 'usuarios';
-  private API_URL : string =  `http://localhost:3000/${this.recurso}`;
+  private API_URL : string = 'http://localhost:3000/usuarios/';
   constructor(private http : HttpClient) { }
 
   login(usuario: UsuarioLogin): Observable<ResultadoGenerico> {
     return this.http.post<ResultadoGenerico>(`${this.API_URL}/iniciarSesion`,usuario);
+  }
+
+  obtenerRol() : Observable<ResultadoGenerico>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`
+    });
+    const requestOptions = { headers: headers };
+    return this.http.get<ResultadoGenerico>(this.API_URL +'/rol',requestOptions);
   }
 }
