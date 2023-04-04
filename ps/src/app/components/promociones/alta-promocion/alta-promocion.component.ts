52783476cd4c3a8c0501e53961def524f6c58192
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-04-04 17:22:04 -0300

    implemento alta promocion

--- ps/src/app/components/promociones/alta-promocion/alta-promocion.component.ts
+++ ps/src/app/components/promociones/alta-promocion/alta-promocion.component.ts
@@ -5,8 +5,10 @@ import { Subscription } from 'rxjs';
 import { DetallePromocion } from 'src/app/models/detalle-promocion';
 import { Producto } from 'src/app/models/producto';
 import { Promocion } from 'src/app/models/promocion';
+import { ResultadoGenerico } from 'src/app/models/resultado-generico';
 import { ProductoService } from 'src/app/services/producto.service';
 import { PromocionService } from 'src/app/services/promocion.service';
+const Swal = require('sweetalert2');
 
 @Component({
   selector: 'app-alta-promocion',
@@ -20,6 +22,8 @@ productos: Producto[];
 detalles: DetallePromocion[] = [];
 
 private subscription = new Subscription();
+controlProductos = new FormControl('');
+cantidadProductos = new FormControl('');
 
 constructor(private servicioPromocion : PromocionService,
            private servicioProducto : ProductoService,
@@ -33,11 +37,77 @@ constructor(private servicioPromocion : PromocionService,
       fechaDesde : [,Validators.required],
       fechaHasta: [,Validators.required],
     });
+    this.cargarCbo();
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
+
+  cargarCbo(): void {
+    this.subscription.add(
+      this.servicioProducto.obtenerTodos().subscribe({
+        next: (res: ResultadoGenerico) => {
+          if (res.ok) {
+            this.productos = res.resultado as Producto[];
+          }
+          else {
+            console.error(res.mensaje)
+          }
+        },
+        error: (err) => { 
+          console.error(err) 
+        }
+      })
+    )
+  }
+
+  agregarProducto(): void {
+    const prod: Producto = this.controlProductos.value as unknown as Producto;
+    if (!prod) return;
+    const cant = this.cantidadProductos.value as unknown as number
+    if (!cant || cant < 1) return;
+    let d: DetallePromocion = {
+      producto: prod,
+      cantidad: cant
+    };
+    let indice = this.productos.indexOf(prod);
+    this.detalles.push(d);
+    this.productos.splice(indice, 1);
+    this.controlProductos.reset();
+    this.cantidadProductos.reset();
+  }
+
+  quitarProducto(detalle: DetallePromocion): void {
+    let indice = this.detalles.indexOf(detalle);
+    this.productos.push(detalle.producto);
+    this.detalles.splice(indice, 1);
+  }
+
+  registrar(){
+    if(this.formulario.valid){
+      this.promocion= this.formulario.value as Promocion;
+      this.promocion.detalles = this.detalles;
+      this.promocion.fechaDesde= new Date(this.formulario.value.fechaDesde);
+      this.promocion.fechaHasta= new Date(this.formulario.value.fechaHasta);
+      this.promocion.fechaHasta.setHours(this.promocion.fechaHasta.getHours() + 23);
+      this.promocion.fechaHasta.setMinutes(this.promocion.fechaHasta.getMinutes() + 59);
+      
+      this.subscription.add(
+        this.servicioPromocion.agregar(this.promocion).subscribe({
+          next: () => {
+            Swal.fire({title:'Listo!', text:`Registro la promoción con éxito`, icon: 'success'});
+          },
+          error: () => {
+            Swal.fire({title:'Error!', text:`Error al registrar promoción`, icon: 'error'});
+          }
+        })
+      )
+    }else{
+      Swal.fire({title: 'Atención!', text: 'Complete todos los campos',icon: 'warning'})
+    }
+  }
+
 
   get controlNombre() : FormControl{
     return this.formulario.controls['nombre'] as FormControl;
@@ -58,5 +128,4 @@ constructor(private servicioPromocion : PromocionService,
   get controlFechaHasta() : FormControl{
     return this.formulario.controls['fechaHasta'] as FormControl;
   }
-
 }
