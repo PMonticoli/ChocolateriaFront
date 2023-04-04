04ad9ffb5bd0c7a5a52e737bd52aaac167bd8769
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-04-04 11:08:19 -0300

    creo e implemento servicio promocion

---
+++ ps/src/app/services/promocion.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Promocion } from '../models/promocion';
+import { ResultadoGenerico } from '../models/resultado-generico';
+
+@Injectable()
+export class PromocionService {
+
+  constructor(private http : HttpClient) { }
+  private API_URL : string = 'http://localhost:3000/promociones/';
+
+  agregar(promocion : Promocion) : Observable<ResultadoGenerico>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`
+    });
+  const requestOptions = { headers: headers };
+   return this.http.post<ResultadoGenerico>(this.API_URL,promocion,requestOptions);
+  }
+
+  obtenerTodas() : Observable<ResultadoGenerico>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`
+    })
+    const requestOptions = { headers : headers };
+    return this.http.get<ResultadoGenerico>(this.API_URL,requestOptions);
+  }
+
+  eliminar(promocion : Promocion) : Observable<ResultadoGenerico>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`
+    })
+    const requestOptions = { headers : headers };
+    return this.http.delete<ResultadoGenerico>(this.API_URL +promocion.id ,requestOptions);
+  }
+
+  obtenerVigentes() : Observable<ResultadoGenerico>{
+    return this.http.get<ResultadoGenerico>(this.API_URL+ 'vigentes');
+  }
+
+  canjear(promocion : Promocion) : Observable<ResultadoGenerico>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      });
+    const requestOptions = { headers: headers };
+    return this.http.post<ResultadoGenerico>(this.API_URL+'canjear',promocion,requestOptions);
+  }
+}
