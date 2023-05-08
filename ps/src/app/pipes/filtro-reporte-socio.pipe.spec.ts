4355a227e8a16e6cc6cb6b6a390ce50bb6dc29f7
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-08 13:20:17 -0300

    Agrego pipe filtro reporte socio

---
+++ ps/src/app/pipes/filtro-reporte-socio.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FiltroReporteSocioPipe } from './filtro-reporte-socio.pipe';
+
+describe('FiltroReporteSocioPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FiltroReporteSocioPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ ps/src/app/pipes/filtro-reporte-socio.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DtoSocios } from '../models/dto-socios';
+
+@Pipe({
+  name: 'filtroReporteSocio'
+})
+export class FiltroReporteSocioPipe implements PipeTransform {
+
+  transform(socios: DtoSocios[], page: number=0, search : string): DtoSocios[] {
+    if(search.length === 0){
+      return socios.slice(page, page+4);
+    }
+    const sociosFiltrados = socios.filter(soc=>  soc.socio.normalize('NFD').toLowerCase()
+    .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1").includes(search) 
+    || soc.dni.toString().includes(search));
+    return sociosFiltrados.slice(page, page+4);
+  }
+
+}
