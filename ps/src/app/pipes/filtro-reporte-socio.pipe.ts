e5154fab1086667910022f48852f77881c0e03d5
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-08 13:33:52 -0300

    Modifico pipe filtro reporte socio

--- ps/src/app/pipes/filtro-reporte-socio.pipe.ts
+++ ps/src/app/pipes/filtro-reporte-socio.pipe.ts
@@ -8,12 +8,12 @@ export class FiltroReporteSocioPipe implements PipeTransform {
 
   transform(socios: DtoSocios[], page: number=0, search : string): DtoSocios[] {
     if(search.length === 0){
-      return socios.slice(page, page+4);
+      return socios.slice(page, page+5);
     }
     const sociosFiltrados = socios.filter(soc=>  soc.socio.normalize('NFD').toLowerCase()
     .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1").includes(search) 
     || soc.dni.toString().includes(search));
-    return sociosFiltrados.slice(page, page+4);
+    return sociosFiltrados.slice(page, page+5);
   }
 
 }
