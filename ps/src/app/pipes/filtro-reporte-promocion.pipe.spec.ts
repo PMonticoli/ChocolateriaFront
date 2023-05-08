4182da3863209e8546c4100aeff1568640224e59
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-08 17:58:27 -0300

    creo pipe filtro reporte promocion

---
+++ ps/src/app/pipes/filtro-reporte-promocion.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FiltroReportePromocionPipe } from './filtro-reporte-promocion.pipe';
+
+describe('FiltroReportePromocionPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FiltroReportePromocionPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ ps/src/app/pipes/filtro-reporte-promocion.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DtoPromociones } from '../models/dto-promociones';
+import { DtoReportePromociones } from '../models/dto-reporte-promociones';
+
+@Pipe({
+  name: 'filtroReportePromocion'
+})
+export class FiltroReportePromocionPipe implements PipeTransform {
+
+  transform(promocion: DtoReportePromociones[], page: number=0, search : string): DtoReportePromociones[] {
+    if(search.length === 0){
+      return promocion.slice(page, page+5);
+    }
+    const promocionesFiltradas = promocion.filter(soc=>  soc.nombrePromocion.normalize('NFD').toLowerCase()
+    .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1").includes(search));
+    return promocionesFiltradas.slice(page, page+5);
+  }
+
+}
