71711ac7ba9eda5605a8b9880e3890890211298d
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-08 09:41:28 -0300

    creo componente reporte productos

---
+++ ps/src/app/components/reportes/reporte-productos/reporte-productos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReporteProductosComponent } from './reporte-productos.component';
+
+describe('ReporteProductosComponent', () => {
+  let component: ReporteProductosComponent;
+  let fixture: ComponentFixture<ReporteProductosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReporteProductosComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ReporteProductosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ps/src/app/components/reportes/reporte-productos/reporte-productos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-productos',
+  templateUrl: './reporte-productos.component.html',
+  styleUrls: ['./reporte-productos.component.css']
+})
+export class ReporteProductosComponent {
+
+}
