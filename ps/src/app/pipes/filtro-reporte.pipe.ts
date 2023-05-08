0ad08ba5e87dc12c4c615b354bac5b5f484f6513
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-08 16:17:04 -0300

    Agregue filtro al pipe reporte producto

--- ps/src/app/pipes/filtro-reporte.pipe.ts
+++ ps/src/app/pipes/filtro-reporte.pipe.ts
@@ -5,13 +5,35 @@ import { DtoReporte } from '../models/dto-reporte';
   name: 'filtroReporte'
 })
 export class FiltroReportePipe implements PipeTransform {
-  transform(producto: DtoReporte[], page: number=0, search : string): DtoReporte[] {
-    if(search.length === 0){
-      return producto.slice(page, page+5);
+  transform(productos: DtoReporte[], page: number = 0, search: string, filtro: string): DtoReporte[] {
+    if (search.length === 0) {
+      return productos.slice(page, page + 5);
     }
-    const productosFiltrados = producto.filter(prod=>  prod.nombre.normalize('NFD').toLowerCase()
-    .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1").includes(search));
-    return productosFiltrados.slice(page, page+5);
+
+    let productosFiltrados = productos.filter(prod => {
+      let valorFiltro: string;
+
+      switch (filtro) {
+        case 'nombre':
+          valorFiltro = prod.nombre;
+          break;
+        case 'cantidad':
+          valorFiltro = prod.cantidad.toString();
+          break;
+        case 'promedio':
+          valorFiltro = prod.promedio.toString();
+          break;
+        default:
+          valorFiltro = '';
+          break;
+      }
+
+      return valorFiltro.normalize('NFD').toLowerCase()
+        .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi, "$1")
+        .includes(search);
+    });
+
+    return productosFiltrados.slice(page, page + 5);
   }
 
 }
