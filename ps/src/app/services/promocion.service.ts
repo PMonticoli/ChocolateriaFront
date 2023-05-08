07da163d56ee3b70bebb8ec73320476723a3cb76
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-08 17:58:13 -0300

    agrego al service el reporte promocion

--- ps/src/app/services/promocion.service.ts
+++ ps/src/app/services/promocion.service.ts
@@ -77,4 +77,17 @@ export class PromocionService {
     const requestOptions = { headers: headers };
     return this.http.get<ResultadoGenerico>(this.API_URL + 'detalles/' + id, requestOptions);
   }
+
+  reportePromociones(body: any): Observable<ResultadoGenerico> {
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+
+        'Content-Type': 'application/json',
+
+        'Authorization': `Bearer ${auth_token}`
+
+      });
+    const requestOptions = { headers: headers };
+    return this.http.post<ResultadoGenerico>(this.API_URL+'reportePromocion',body,requestOptions);
+  }
 }
