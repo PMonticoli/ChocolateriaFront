622875e90bda187235e7037571f30321f2907800
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-08 11:16:22 -0300

    creo dto reporte productos

---
+++ ps/src/app/models/dto-reporte.spec.ts
@@ -0,0 +1,7 @@
+import { DtoReporte } from './dto-reporte';
+
+describe('DtoReporte', () => {
+  it('should create an instance', () => {
+    expect(new DtoReporte()).toBeTruthy();
+  });
+});
---
+++ ps/src/app/models/dto-reporte.ts
@@ -0,0 +1,10 @@
+import { DetallePedido } from "./detalle-pedido";
+import { Producto } from "./producto";
+
+export class DtoReporte {
+    id : number;
+    producto : Producto;
+    nombre : string;
+    cantidad : number;
+    promedio : number;
+}
