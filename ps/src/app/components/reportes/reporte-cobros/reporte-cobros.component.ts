684b196f08ca88b89eb6793f775f5c8c79311743
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-15 09:00:46 -0300

    creo componente reporte cobros

---
+++ ps/src/app/components/reportes/reporte-cobros/reporte-cobros.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-cobros',
+  templateUrl: './reporte-cobros.component.html',
+  styleUrls: ['./reporte-cobros.component.css']
+})
+export class ReporteCobrosComponent {
+
+}
