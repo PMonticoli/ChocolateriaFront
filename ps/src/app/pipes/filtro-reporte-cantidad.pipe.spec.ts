387d76d970bb802580172a99cdfb333408436cd0
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-22 12:47:15 -0300

    agrego nuevo pipe filtro reporte producto

---
+++ ps/src/app/pipes/filtro-reporte-cantidad.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FiltroReporteCantidadPipe } from './filtro-reporte-cantidad.pipe';
+
+describe('FiltroReporteCantidadPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FiltroReporteCantidadPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ ps/src/app/pipes/filtro-reporte-cantidad.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DtoReporte } from '../models/dto-reporte';
+
+@Pipe({
+  name: 'filtroReporteCantidad'
+})
+export class FiltroReporteCantidadPipe implements PipeTransform {
+
+  transform(productos: DtoReporte[], page : number= 0, search : string): DtoReporte[] {
+    if(search.length=== 0){
+      return productos.slice(page, page+4);
+    } 
+
+    const productosFiltrados = productos.filter(prod => prod.nombre.normalize('NFD').toLowerCase()
+    .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1").includes(search));
+    return productosFiltrados.slice(page, page+4);
+  }
+
+}
