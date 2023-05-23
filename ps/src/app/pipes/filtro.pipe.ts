2fded8e299ed89e0fe9bfbd20948e59e2cb8738e
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-23 17:08:36 -0300

    Modifico pipe filtro producto

--- ps/src/app/pipes/filtro.pipe.ts
+++ ps/src/app/pipes/filtro.pipe.ts
@@ -8,12 +8,12 @@ export class FiltroPipe implements PipeTransform {
 
   transform(producto: Producto[], page : number= 0, search : string): Producto[] {
     if(search.length=== 0){
-      return producto.slice(page, page+6);
+      return producto.slice(page, page+5);
     } 
 
     const productosFiltrados = producto.filter(prod => prod.nombre.normalize('NFD').toLowerCase()
     .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1").includes(search));
-    return productosFiltrados.slice(page, page+6);
+    return productosFiltrados.slice(page, page+5);
   }
 
 }
