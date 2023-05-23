38a49b23cd5e8a89feb1504109cfce6e0d3ff5ab
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-05-23 12:13:34 -0300

    agrego guard admin para manejo de permisos

---
+++ ps/src/app/guards/admin.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import Swal from 'sweetalert2';
+import { UsuarioService } from '../services/usuario.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(private router: Router, private servicioUsuario : UsuarioService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const body = token.split('.')[1];
+      const buff = JSON.parse(atob(body));
+      if (buff.rol === 'Admin') {
+        return true;
+      } else {
+        Swal.fire({
+          title: 'ERROR!',
+          text: 'No tienes los permisos adecuados para acceder a este recurso',
+          icon: 'error'
+        });
+        return this.router.parseUrl('/home');
+      }
+    } 
+    else {
+      return this.router.parseUrl('/login');
+    }
+  }
+  
+}
