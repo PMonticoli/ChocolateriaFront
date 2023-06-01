e6b537874645b8e4238e10708087239c0f7e0807
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-06-01 17:01:58 -0300

    modifico pipe reporte de productos

--- ps/src/app/pipes/filtro-reporte.pipe.ts
+++ ps/src/app/pipes/filtro-reporte.pipe.ts
@@ -7,12 +7,12 @@ import { DtoReporte } from '../models/dto-reporte';
 export class FiltroReportePipe implements PipeTransform {
   transform(productos: DtoReporte[], page : number= 0, search : string): DtoReporte[] {
     if(search.length=== 0){
-      return productos.slice(page, page+4);
+      return productos.slice(page, page+8);
     } 
 
     const productosFiltrados = productos.filter(prod => prod.nombre.normalize('NFD').toLowerCase()
     .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1").includes(search));
-    return productosFiltrados.slice(page, page+4);
+    return productosFiltrados.slice(page, page+8);
   }
 
 }
