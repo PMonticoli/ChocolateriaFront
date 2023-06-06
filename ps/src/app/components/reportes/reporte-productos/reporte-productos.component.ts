c91f2505fb264178abd53b50bcd4a3db30124a3e
2w1-113491-Monticoli Pablo <90335378+PMonticoli@users.noreply.github.com>
2023-06-06 19:56:46 -0300

    descargar pdf tabla con varias paginas reporte

--- ps/src/app/components/reportes/reporte-productos/reporte-productos.component.ts
+++ ps/src/app/components/reportes/reporte-productos/reporte-productos.component.ts
@@ -3,11 +3,10 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Subscription } from 'rxjs';
 import { ProductoService } from 'src/app/services/producto.service';
 const Swal = require('sweetalert2');
-import html2canvas from 'html2canvas';
-import { jsPDF } from "jspdf";
 import { ResultadoGenerico } from 'src/app/models/resultado-generico';
 import { DtoReporte } from 'src/app/models/dto-reporte';
-
+import jsPDF from 'jspdf'
+import autoTable from 'jspdf-autotable'
 @Component({
   selector: 'app-reporte-productos',
   templateUrl: './reporte-productos.component.html',
@@ -39,49 +38,15 @@ export class ReporteProductosComponent implements OnInit, OnDestroy{
     this.subscription.unsubscribe();
   }
 
-  private getCantidad() {
-    if (!this.formulario.valid) {
-      Swal.fire({title:'Atención!', text:'¡Debes seleccionar previamente una fecha desde y hasta para generar el reporte!', icon: 'warning'});
-      return;
-    }
-    this.visibilidadReporte = true;
-    if (this.formulario.valid) {
-      const {fechaDesde, fechaHasta} = this.formulario.value;
-      this.body = {
-        fechaDesde: new Date(fechaDesde),
-        fechaHasta: new Date(fechaHasta)
-      };
-      this.body.fechaHasta.setHours(this.body.fechaHasta.getHours() + 23);
-      this.body.fechaHasta.setMinutes(this.body.fechaHasta.getMinutes() + 59);
-  
-      this.subscription.add(
-        this.servicioProducto.reporteCantidad(this.body).subscribe({
-          next: (res: ResultadoGenerico) => {
-            if (res.ok) {
-              this.cantidadProd = res.resultado ? res.resultado[0].cantidadProd : 0;
-              this.resultadoCantidad = res.resultado ? res.resultado : [];
-            } else {
-              console.error(res.mensaje);
-            }
-          },
-          error: (err) => {
-            console.error(err);
-          }
-        })
-      );
-    } else {
-      Swal.fire({title:'Atención!', text:'¡Completar campos de fechas!', icon: 'warning'});
-    }
-  }
-  
+
   private getPromedio() {
     if (!this.formulario.valid) {
-      Swal.fire({title:'Atención!', text:'¡Debes seleccionar previamente una fecha desde y hasta para generar el reporte!', icon: 'warning'});
+      Swal.fire({ title: 'Atención!', text: '¡Debes seleccionar previamente una fecha desde y hasta para generar el reporte!', icon: 'warning' });
       return;
     }
-    this.visibilidadReporte = true;
+  
     if (this.formulario.valid) {
-      const {fechaDesde, fechaHasta} = this.formulario.value;
+      const { fechaDesde, fechaHasta } = this.formulario.value;
       this.body = {
         fechaDesde: new Date(fechaDesde),
         fechaHasta: new Date(fechaHasta)
@@ -93,9 +58,9 @@ export class ReporteProductosComponent implements OnInit, OnDestroy{
         this.servicioProducto.reportePromedio(this.body).subscribe({
           next: (res: ResultadoGenerico) => {
             if (res.ok) {
-              if(this.body.fechaDesde> this.body.fechaHasta){
-                Swal.fire({title : 'Atención!', text:`Ingrese fechas validas:`, icon: 'warning'});
-                this.visibilidadReporte= false;
+              if (this.body.fechaDesde > this.body.fechaHasta) {
+                Swal.fire({ title: 'Atención!', text: 'Ingrese fechas válidas:', icon: 'warning' });
+                this.visibilidadReporte = false;
                 return;
               }
               this.promedioProd = res.resultado ? res.resultado[0].promedioProd : 0;
@@ -110,72 +75,138 @@ export class ReporteProductosComponent implements OnInit, OnDestroy{
         })
       );
     } else {
-      Swal.fire({title:'Atención!', text:'¡Completar campos de fechas!', icon: 'warning'});
-    }
-  }
-
-  solicitarReporte() {
-    if (this.formulario.valid) {
-      this.visibilidadReporte = true;
-      this.getCantidad();
-      this.getPromedio();
-    } else {
-      Swal.fire({title:'Atención!', text:'¡Debes seleccionar previamente una fecha desde y hasta para generar el reporte!', icon: 'warning'});
+      Swal.fire({ title: 'Atención!', text: '¡Completar campos de fechas!', icon: 'warning' });
     }
   }
   
-  
 
-  
-  descargarPDF(): void {
-    let DATA: any = document.getElementById('htmlData');
-    html2canvas(DATA).then((canvas) => {
-      let ancho = 290;
-      let altura = (canvas.height * ancho) / canvas.width;
-      const urlArchivo = canvas.toDataURL('image/png');
-      let ArchivoPDF = new jsPDF('l', 'mm', 'a4');
-      let position = 0;
-      let pageHeight = ArchivoPDF.internal.pageSize.getHeight();
-  
-      if (altura > pageHeight - 20) {
-        altura = pageHeight - 20; 
-      }
-  
-      ArchivoPDF.addImage(urlArchivo, 'PNG', 10, 10, ancho, altura);
-  
-      console.log(new Date().toLocaleDateString("es-AR"));
-      ArchivoPDF.save(`Reporte Productos (${new Date().toLocaleDateString("es-AR")}).pdf`);
-    });
-  }
-  
-  
-
-  get controlFechaDesde () : FormControl{
-    return this.formulario.controls['fechaDesde'] as FormControl;
+private getCantidad(generarPDF = false) {
+  if (!this.formulario.valid) {
+    Swal.fire({ title: 'Atención!', text: '¡Debes seleccionar previamente una fecha desde y hasta para generar el reporte!', icon: 'warning' });
+    return;
   }
 
-  get controlFechaHasta () : FormControl{
-    return this.formulario.controls['fechaHasta'] as FormControl;
-  }
+  if (this.formulario.valid) {
+    const { fechaDesde, fechaHasta } = this.formulario.value;
+    this.body = {
+      fechaDesde: new Date(fechaDesde),
+      fechaHasta: new Date(fechaHasta)
+    };
+    this.body.fechaHasta.setHours(this.body.fechaHasta.getHours() + 23);
+    this.body.fechaHasta.setMinutes(this.body.fechaHasta.getMinutes() + 59);
 
-  nextPage(){
+    this.subscription.add(
+      this.servicioProducto.reporteCantidad(this.body).subscribe({
+        next: (res: ResultadoGenerico) => {
+          if (res.ok) {
+            this.cantidadProd = res.resultado ? res.resultado[0].cantidadProd : 0;
+            this.resultadoCantidad = res.resultado ? res.resultado : [];
+
+            if (generarPDF) {
+              this.generarReportePDF();
+            }
+          } else {
+            console.error(res.mensaje);
+          }
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      })
+    );
+  } else {
+    Swal.fire({ title: 'Atención!', text: '¡Completar campos de fechas!', icon: 'warning' });
+  }
+}
+
+solicitarReporte() {
+  if (this.formulario.valid) {
+    this.visibilidadReporte = true;
+    this.getCantidad();
+    this.getPromedio();
+  } else {
+    Swal.fire({title:'Atención!', text:'¡Debes seleccionar previamente una fecha desde y hasta para generar el reporte!', icon: 'warning'});
+  }
+}
+
+
+generarReportePDF() {
+  const doc = new jsPDF('p', 'mm', 'a4');
+  const elementosTablaCantidad = this.obtenerElementosCantidadTabla();
+  const elementosTablaPromedio = this.obtenerElementosPromedioTabla();
+
+  const cantidadTituloY = 20;
+  const promedioTituloY = cantidadTituloY + elementosTablaCantidad.length * 10 + 10;
+  
+  doc.setFontSize(20);
+  doc.text('Reporte de Productos', 10, 10);
+  
+  doc.setFontSize(14);
+  doc.text('Cantidad de unidades vendidas por producto', 10, cantidadTituloY);
+  autoTable(doc, { body: elementosTablaCantidad, startY: cantidadTituloY + 10 });
+
+  doc.text('Promedio de unidades vendidas por pedido', 10, promedioTituloY);
+  autoTable(doc, { body: elementosTablaPromedio, startY: promedioTituloY + 10 });
+
+  console.log(new Date().toLocaleDateString("es-AR"));
+  doc.save(`Reporte Productos (${new Date().toLocaleDateString("es-AR")}).pdf`);
+}
+
+
+
+obtenerElementosCantidadTabla(): string[][] {
+  const elementos: string[][] = [];
+  this.resultadoCantidad.forEach((res) => {
+    const datosFila: string[] = [res.nombre, res.cantidad.toString()];
+    elementos.push(datosFila);
+  });
+  return elementos;
+}
+
+obtenerElementosPromedioTabla(): string[][] {
+  const elementos: string[][] = [];
+  this.resultadoPromedio.forEach((res) => {
+    const datosFila: string[] = [res.nombre, res.promedio.toString()];
+    elementos.push(datosFila);
+  });
+  return elementos;
+}
+
+
+
+descargarPDF() {
+  this.getCantidad(true);
+  this.getPromedio();
+}
+
+
+
+get controlFechaDesde () : FormControl{
+  return this.formulario.controls['fechaDesde'] as FormControl;
+}
+
+get controlFechaHasta () : FormControl{
+  return this.formulario.controls['fechaHasta'] as FormControl;
+}
+
+nextPage(){
     this.page+=8;
   }
 
-  prevPage(){
-    if(this.page>0){
-      this.page-=8;
+prevPage(){
+  if(this.page>0){
+    this.page-=8;
     }
   }
 
-  pageProm : number=0;
-  nextPageProm(){
-    this.pageProm+=8;
-  }
+pageProm : number=0;
+nextPageProm(){
+  this.pageProm+=8;
+}
 
   prevPageProm(){
-    if(this.pageProm>0){
-      this.pageProm-=8;
+   if(this.pageProm>0){
+    this.pageProm-=8;
     }
   }
 
